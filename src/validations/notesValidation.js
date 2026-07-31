bf8c64dce8a8f32360d9002e0320ce6fd396346f
Orest-Stetsyk <orest.stetsik@gmail.com>
2026-07-31 15:31:40 +0300

    fix

--- src/validations/notesValidation.js
+++ src/validations/notesValidation.js
@@ -8,7 +8,12 @@ const objectIdValidator = (value, helpers) => {
 
 export const noteIdSchema = {
   [Segments.PARAMS]: Joi.object({
-    noteId: Joi.string().custom(objectIdValidator).required(),
+    noteId: Joi.string().custom(objectIdValidator).required().messages({
+    'any.invalid': 'noteId має бути коректним MongoDB ObjectId',
+    'string.empty': 'noteId має бути коректним MongoDB ObjectId',
+    'string.base': 'noteId має бути коректним MongoDB ObjectId',
+    'any.required': 'noteId є обов’язковим',
+  })
   }),
 };
 
@@ -18,22 +23,24 @@ export const updateNoteSchema = {
   }),
   [Segments.BODY]: Joi.object({
     title: Joi.string().min(1).max(100),
-    content: Joi.string(),
+    content: Joi.string().allow(''),
     tag: Joi.string().valid(...TAGS),
   })
+  .or('title', 'content', 'tag')
+
 };
 export const createNoteSchema = {
   [Segments.BODY]: Joi.object({
     title: Joi.string().min(1).required(),
-    content: Joi.string(),
+    content: Joi.string().allow(''),
     tag: Joi.string().valid(...TAGS),
   }),
 };
 
 export const getAllNotesSchema = {
   [Segments.QUERY]: Joi.object({
-    page: Joi.number().integer().min(1),
-    perPage: Joi.number().integer().min(5).max(20),
+    page: Joi.number().integer().min(1).default(1),
+    perPage: Joi.number().integer().min(5).max(20).default(10),
     tag: Joi.string().valid(...TAGS),
     search: Joi.string()
   })
